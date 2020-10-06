78b679c1023a7871ce8e663c29f936d10c431b54
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-06 11:21:44 +0700

    Create an empty list of firstname and lastname

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
-  const [inputNameList, setInputNameList] = useState(
-    [
-      {
-        firstname: '',
-        lastname: ''
-      }
-    ]
-  )
+  // list contains state for firstname and lastname
+  const [inputNameList, setInputNameList] = useState([{ firstname: '', lastname: '' }])
   return (
     <div className="App">
     </div>
