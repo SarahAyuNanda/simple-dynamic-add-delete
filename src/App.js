29cc0ebf8ebb1240daa1044a46d8896219d97f62
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-07 12:36:24 +0700

    Create average

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   // list contains state for firstname and age
-  const [inputInfoList, setInputInfoList] = useState([{ firstname: '', age: 0 }])
+  const [inputInfoList, setInputInfoList] = useState([{ firstname: '', age: "" }])
 
   // handle change to input info
   const handleInputChange = (event, index) => {
@@ -24,24 +24,36 @@ function App() {
 
   // handle click for add button
   const handleAddClick = () => {
-    setInputInfoList([...inputInfoList, { firstname: "", age: 0 }])
+    setInputInfoList([...inputInfoList, { firstname: "", age: "" }])
+  }
+
+  // handle change to count the average 
+  const ageAverage = () => {
+    const sum = inputInfoList.reduce((beforeSum, currentSum) => beforeSum = beforeSum + parseInt(currentSum.age), 0)
+    const length = inputInfoList.length
+    const average = sum / length
+    return average
   }
 
   return (
     <div className="App">
+      {/* ADD BUTTON */}
       <button type="button" class="btn btn-primary" onClick={handleAddClick}>Add</button>
+
+      {/* INPUT FORM */}
       {inputInfoList.map((data, index) => {
         return (
           <div className="Form">
             <input
               type="text"
+              autoComplete="off"
               placeholder="Firstname"
               class="form-control"
               name="firstname"
               value={data.firstname}
               onChange={e => handleInputChange(e, index)} />
             <input
-              type="text"
+              type="number"
               placeholder="Age"
               class="form-control"
               name="age"
@@ -51,11 +63,18 @@ function App() {
           </div>
         )
       })}
+
+      {/* SHOW AVERAGE OF AGE */}
+      <div className="Average">
+        Average of age : {inputInfoList.length === 0 ? 0 : ageAverage()}
+      </div>
+
+      {/* TABLE */}
       <div class="table-responsive-sm">
         <table class="table table-bordered table-hover">
           <thead>
-            <tr>
-              <th scope="col">#</th>
+            <tr align="center">
+              <th scope="col" style={{ width: "50px" }}>#</th>
               <th scope="col">Firstname</th>
               <th scope="col">Age</th>
             </tr>
@@ -66,7 +85,7 @@ function App() {
                 <th colSpan="3" >...No showed data...</th>
               </tr> :
               inputInfoList.map((data, index) => (
-                <tr key={index}>
+                <tr key={index} align="center">
                   <td>{index + 1}</td>
                   <td>{data.firstname}</td>
                   <td>{data.age}</td>
