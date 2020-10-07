01d71db3de9ef6e02adb167bf9e429db5af1f95b
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-07 16:06:30 +0700

    Create convert first char to uppercase

--- src/App.js
+++ src/App.js
@@ -4,68 +4,65 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 class App extends Component {
-  state = [
-    {
-      firstname: 'Sarah',
-      age: '24'
-    }
-  ]
+  state = {
+    data: []
+  }
 
   handleInputChange = (event, index) => {
     const { name, value } = event.target
-    const infoList = [...this.state]
+    const infoList = [...this.state.data]
     infoList[index][name] = value
-    this.setState({ ...infoList })
+    this.setState({ infoList })
   }
 
   handleAddClick = () => {
-    this.setState([...this.state, { firstname: '', age: '' }])
+    const allData = this.state.data
+    const addData = allData.push({firstname: '', age: ''})
+    this.setState([...this.state.data, addData])
   }
 
   handleDeleteClick = index => {
-    const infoList = [...this.state]
-    infoList.splice(index, 1)
-    this.setState({ ...infoList })
+    const allData = this.state.data
+    const deleteData = allData.splice(index, 1)
+    this.setState([...this.state.data, deleteData])
   }
 
   ageAverage = () => {
-    const sum = this.state.reduce((beforeSum, currentSum) => beforeSum = beforeSum + parseInt(currentSum.age), 0)
-    const length = this.state.length
+    const sum = this.state.data.reduce((beforeSum, currentSum) => beforeSum = beforeSum + parseInt(currentSum.age), 0)
+    const length = this.state.data.length
     const average = sum / length
     return average
   }
 
-  addFormInput = () => {
-    return this.state.map((data, index) => (
-      <div className="Form">
-        <input
-          type="text"
-          autoComplete="off"
-          placeholder="Firstname"
-          class="form-control"
-          name="firstname"
-          value={data.firstname}
-          onChange={e => this.handleInputChange(e, index)} />
-        <input
-          type="number"
-          placeholder="Age"
-          class="form-control"
-          name="age"
-          value={data.age}
-          onChange={e => this.handleInputChange(e, index)} />
-        <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteClick(index)}>Delete</button>
-      </div>
-    ))
+  changeToLetterCase = (param) => {
+    return param.charAt(0).toUpperCase() + param.slice(1)
   }
 
   render() {
-
-
     return (
       <div className="App">
         <button type="button" class="btn btn-primary" onClick={this.handleAddClick}>Add</button>
 
-        {this.addFormInput()}
+        {this.state.data.map((data, index) => (
+          <div className="Form">
+            <input
+              type="text"
+              autoComplete="off"
+              placeholder="Firstname"
+              class="form-control"
+              name="firstname"
+              value={data.firstname}
+              onChange={e => this.handleInputChange(e, index)} />
+            <input
+              type="number"
+              placeholder="Age"
+              class="form-control"
+              name="age"
+              value={data.age}
+              onChange={e => this.handleInputChange(e, index)} />
+            <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteClick(index)}>Delete</button>
+          </div>
+        ))}
 
         <div className="Average">
           Average of age : {this.ageAverage()}
@@ -73,18 +70,18 @@ class App extends Component {
 
         <div class="table-responsive-sm">
           <table class="table table-bordered table-hover">
-            <thead>
+            <thead class="thead-dark">
               <tr align="center">
                 <th scope="col" style={{ width: "50px" }}>#</th>
-                <th scope="col">Firstname</th>
-                <th scope="col">Age</th>
+                <th scope="col" style={{ width: "fit-content" }}>Firstname</th>
+                <th scope="col" style={{ width: "fit-content" }}>Age</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.map((data, index) => (
+              {this.state.data.map((data, index) => (
                 <tr align="center">
                   <td>{index + 1}</td>
-                  <td>{data.firstname}</td>
+                  <td>{this.changeToLetterCase(data.firstname)}</td>
                   <td>{data.age}</td>
                 </tr>
               ))}
