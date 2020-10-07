1e66c8b630b178f885584758322f5414d079d995
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-07 10:30:10 +0700

    Create handle to add and delete

--- src/App.js
+++ src/App.js
@@ -5,33 +5,52 @@ import './App.css';
 
 function App() {
   // list contains state for firstname and age
-  const [inputInfoList, setInputInfoist] = useState([{ firstname: '', age: 0 }])
+  const [inputInfoList, setInputInfoList] = useState([{ firstname: '', age: 0 }])
 
-  const addInfo = () => {
-    inputInfoList.map((data, index) => {
-      return (
-        <div className="Form">
-          <input
-            type="text"
-            placeholder="Firstname"
-            class="form-control"
-            name="firstname"
-            value={data.firstname} />
-          <input
-            type="number"
-            placeholder="Age"
-            class="form-control"
-            name="age"
-            value={data.age} />
-          <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-      )
-    })
+  // handle change to input info
+  const handleInputChange = (event, index) => {
+    const { name, value } = event.target
+    const infoList = [...inputInfoList]
+    infoList[index][name] = value
+    setInputInfoList(infoList)
+  }
+
+  // handle click for delete button
+  const handleDeleteClick = index => {
+    const infoList = [...inputInfoList]
+    infoList.splice(index, 1)
+    setInputInfoList(infoList)
+  }
+
+  // handle click for add button
+  const handleAddClick = () => {
+    setInputInfoList([...inputInfoList, { firstname: "", age: 0 }])
   }
 
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary" onClick={addInfo()}>Add</button>
+      <button type="button" class="btn btn-primary" onClick={handleAddClick}>Add</button>
+      {inputInfoList.map((data, index) => {
+        return (
+          <div className="Form">
+            <input
+              type="text"
+              placeholder="Firstname"
+              class="form-control"
+              name="firstname"
+              value={data.firstname}
+              onChange={e => handleInputChange(e, index)} />
+            <input
+              type="number"
+              placeholder="Age"
+              class="form-control"
+              name="age"
+              value={data.age}
+              onChange={e => handleInputChange(e, index)} />
+            <button type="button" class="btn btn-danger" onClick={() => handleDeleteClick(index)}>Delete</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
