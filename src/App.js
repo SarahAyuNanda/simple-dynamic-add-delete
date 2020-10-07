70b0ae44b70ab2fecf4370ae83d1bce67e63236f
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-07 10:09:05 +0700

    Create function to add info

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import './App.css';
 function App() {
   // list contains state for firstname and age
   const [inputInfoList, setInputInfoist] = useState([{ firstname: '', age: 0 }])
+
+  const addInfo = () => {
+    inputInfoList.map((data, index) => {
+      return (
+        <div className="Form">
+          <input
+            type="text"
+            placeholder="Firstname"
+            class="form-control"
+            name="firstname"
+            value={data.firstname} />
+          <input
+            type="number"
+            placeholder="Age"
+            class="form-control"
+            name="age"
+            value={data.age} />
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <div className="Form">
-        <input type="text" placeholder="Firstname" class="form-control" />
-        <input type="text" placeholder="Lastname" class="form-control" />
-      </div>
+      <button type="button" class="btn btn-primary" onClick={addInfo()}>Add</button>
     </div>
   );
 }
