00a5478bf146ac62bcf34253a723604ea439de9f
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-11-03 22:16:01 +0700

    Create table for person list

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   handleAddClick = () => {
     const allData = this.state.data
-    const addData = allData.push({firstname: '', age: ''})
+    const addData = allData.push({ firstname: '', age: 0 })
     this.setState([...this.state.data, addData])
   }
 
@@ -41,7 +41,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button type="button" class="btn btn-primary" onClick={this.handleAddClick}>Add</button>
+        <button type="button" class="btn btn-primary" onClick={this.handleAddClick}>
+          <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+          </svg>
+        </button>
 
         {this.state.data.map((data, index) => (
           <div className="Form">
@@ -55,12 +59,17 @@ class App extends Component {
               onChange={e => this.handleInputChange(e, index)} />
             <input
               type="number"
+              min="0"
               placeholder="Age"
               class="form-control"
               name="age"
               value={data.age}
               onChange={e => this.handleInputChange(e, index)} />
-            <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteClick(index)}>Delete</button>
+            <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteClick(index)}>
+              <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-person-x-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z" />
+              </svg>
+            </button>
           </div>
         ))}
 
@@ -78,13 +87,17 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.data.map((data, index) => (
+              {this.state.data.length === 0 ?
                 <tr align="center">
-                  <td>{index + 1}</td>
-                  <td>{this.changeToLetterCase(data.firstname)}</td>
-                  <td>{data.age}</td>
-                </tr>
-              ))}
+                  <th colSpan="3" >...</th>
+                </tr> :
+                this.state.data.map((data, index) => (
+                  <tr align="center">
+                    <td>{index + 1}</td>
+                    <td>{this.changeToLetterCase(data.firstname)}</td>
+                    <td>{data.age}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
