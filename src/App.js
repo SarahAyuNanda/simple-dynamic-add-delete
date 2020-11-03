af81dcdd7c6e6a917725be01122a4d6b1545508d
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-11-03 22:32:16 +0700

    Leave the note in component

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   handleAddClick = () => {
     const allData = this.state.data
-    const addData = allData.push({ firstname: '', age: 0 })
+    const addData = allData.push({ firstname: '', age: '' })
     this.setState([...this.state.data, addData])
   }
 
@@ -41,12 +41,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* Button to add form input firstname and age */}
         <button type="button" class="btn btn-primary" onClick={this.handleAddClick}>
           <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
           </svg>
         </button>
 
+        {/* Create form as an item in array */}
         {this.state.data.map((data, index) => (
           <div className="Form">
             <input
@@ -65,6 +67,7 @@ class App extends Component {
               name="age"
               value={data.age}
               onChange={e => this.handleInputChange(e, index)} />
+              {/* Button to delete item by its index */}
             <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteClick(index)}>
               <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-person-x-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z" />
@@ -73,10 +76,12 @@ class App extends Component {
           </div>
         ))}
 
+        {/* Count average of all ages by the array */}
         <div className="Average">
           Average of age : {this.ageAverage()}
         </div>
 
+        {/* Show all the input in the table */}
         <div class="table-responsive-sm">
           <table class="table table-bordered table-hover">
             <thead class="thead-dark">
@@ -87,10 +92,12 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
+              {/* Table condition when array is empty */}
               {this.state.data.length === 0 ?
                 <tr align="center">
                   <th colSpan="3" >...</th>
                 </tr> :
+                // Table condition when array has content
                 this.state.data.map((data, index) => (
                   <tr align="center">
                     <td>{index + 1}</td>
